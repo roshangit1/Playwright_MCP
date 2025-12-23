970a0d057533acc228186aeb9d450f0ada681990
Roshan Nair <roshannair89@gmail.com>
2025-12-23 00:03:05 -0700

    Added multi browser support and screenshot/video

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,23 @@ export default defineConfig({
   testDir: "./tests",
   reporter: [["html", { open: "never" }], ["list"]],
   use: {
-    //baseURL: "https://the-internet.herokuapp.com",
-    trace: "on-first-retry"
-  }
+     trace: "on-first-retry",
+     headless: true,
+     screenshot: 'on',
+     video: 'on',
+  },
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    },
+  ],
 });
